0576752fb16caa504ef3785afb15ebf92ee45d8d
ByteZ1337 <0xbytez@gmail.com>
2023-12-30 00:40:10 +0100

    fix: redundant loop and role not being removed when unlinking in dc

--- src/main/java/me/truemb/discordnotify/discord/commands/DC_VerifyCommand.java
+++ src/main/java/me/truemb/discordnotify/discord/commands/DC_VerifyCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import net.dv8tion.jda.api.entities.Guild;
 import org.spicord.api.addon.SimpleAddon;
 import org.spicord.bot.DiscordBot;
 import org.spicord.bot.command.DiscordBotCommand;
@@ -32,6 +33,7 @@ public class DC_VerifyCommand extends SimpleAddon {
     @Override
     public void onCommand(DiscordBotCommand command, String[] args) {
     	Member member = command.getSender();
+		Guild guild = member.getGuild();
     	
     	long disUUID = member.getUser().getIdLong();
     	long channelID = command.getChannel().getIdLong();
@@ -67,70 +69,66 @@ public class DC_VerifyCommand extends SimpleAddon {
     		command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.wrongCommand", placeholder));
     		return;
     	}
-
-    	for(Role role : member.getRoles()){
-    		if(role.getName().equalsIgnoreCase(this.instance.getConfigManager().getConfig().getString("Options." + FeatureType.Verification.toString() + ".discordRole"))) {
-    			
-    	    	if(args[0].equalsIgnoreCase("unlink")) {
-    	    		//UNLINK FROM DISCORD
-    	    		
-    	    		if(this.instance.getVerifyManager().isVerified(disUUID)) {
-    	    			UUID mcuuid = this.instance.getVerifyManager().getVerfiedWith(disUUID);
-    	    			if(mcuuid != null) {
-
-    	    				//REMOVE VERIFY ROLE
-    	    				List<Role> verifyRoles = this.instance.getDiscordManager().getDiscordBot().getJda().getRolesByName(this.instance.getConfigManager().getConfig().getString("Options." + FeatureType.Verification.toString() + ".discordRole"), true);
-    	    				if(verifyRoles.size() > 0) {
-	    	    				Role verifyRole = verifyRoles.get(0);
-	    	    				verifyRole.getGuild().removeRoleFromMember(member, verifyRole).complete();
-    	    				}
-    	    				
-    	    				//NICKNAME
-    	    				if(this.instance.getConfigManager().getConfig().getBoolean("Options." + FeatureType.Verification.toString() + ".changeNickname")) {
-    	    					try {
-    	    						member.modifyNickname(null).complete();
-    	    					}catch(HierarchyException ex) {
-    	    						this.instance.getUniversalServer().getLogger().warning("User " + member.getUser().getAsTag() + " has higher rights, than the BOT! Cant reset the Nickname.");
-    	    					}
-    	    				}
-    	    				
-    	    				this.instance.getDiscordManager().resetRoles(mcuuid, member);
-    	    				
-    	    				String verifyGroupS = this.instance.getConfigManager().getConfig().getString("Options." + FeatureType.Verification.toString() + ".minecraftRank");
-    	    				
-    	    				if(verifyGroupS != null && !verifyGroupS.equalsIgnoreCase("")) {
-    	    					
-    	    					String[] array = verifyGroupS.split(":");
-    	    				
-    	    					if(array.length == 2) {
-    	    						String minecraftRank = array[1];
-
-    	    						if(this.instance.getUniversalServer().isProxy() && array[0].equalsIgnoreCase("s") || this.instance.getPermsAPI().usePluginBridge) {
-    	    							String[] groups = { minecraftRank };
-    	    							this.instance.getPluginMessenger().sendGroupAction(mcuuid, GroupAction.REMOVE, groups);
-    	    						}else {
-    	    							this.instance.getPermsAPI().removeGroup(mcuuid, minecraftRank);
-    	    						}
-    	    						
-    	    					}else {
-    	    						this.instance.getUniversalServer().getLogger().warning("Something went wrong with removing the Verificationsgroup on Minecraft!");
-    	    					}
-    	    				}
-    						
-    	    				this.instance.getVerifyManager().removeVerified(mcuuid);
-    	    				this.instance.getVerifySQL().deleteVerification(disUUID);
-    	    	    		command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.unlinked", placeholder));
-    	    	    		return;
-    	    			}
-    	    		}
-    	    		command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.notVerified", placeholder));
-    	    	}else {
-    	    		//VERIFIED AND TRIED IT AGAIN
-    	    		command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.discordAlreadyAuthenticated", placeholder));
-    	    	}
-	    		return;
-    		}
-    	}
+		
+		var roles = guild.getRolesByName(this.instance.getConfigManager().getConfig().getString("Options." + FeatureType.Verification.toString() + ".discordRole"), true);
+		if (!roles.isEmpty() && member.getRoles().contains(roles.get(0))) {
+			if(args[0].equalsIgnoreCase("unlink")) {
+				
+				// Pretty much same code as in DN_VerifyCommand, refactor later
+				if(this.instance.getVerifyManager().isVerified(disUUID)) {
+					UUID mcuuid = this.instance.getVerifyManager().getVerfiedWith(disUUID);
+					if(mcuuid != null) {
+						
+						//REMOVE VERIFY ROLE
+						guild.removeRoleFromMember(member, roles.get(0)).complete();
+						
+						//NICKNAME
+						if(this.instance.getConfigManager().getConfig().getBoolean("Options." + FeatureType.Verification.toString() + ".changeNickname")) {
+							try {
+								member.modifyNickname(null).complete();
+							}catch(HierarchyException ex) {
+								this.instance.getUniversalServer().getLogger().info("User " + member.getUser().getAsTag() + " has higher rights, than the BOT! Cant change the Nickname.");
+							}
+						}
+						
+						//RESET ROLES
+						this.instance.getDiscordManager().resetRoles(mcuuid, member);
+						
+						String verifyGroupS = this.instance.getConfigManager().getConfig().getString("Options." + FeatureType.Verification.toString() + ".minecraftRank");
+						
+						if(verifyGroupS != null && !verifyGroupS.equalsIgnoreCase("")) {
+							
+							String[] array = verifyGroupS.split(":");
+							
+							if(array.length == 2) {
+								String minecraftRank = array[1];
+								
+								if(this.instance.getUniversalServer().isProxy() && array[0].equalsIgnoreCase("s") || this.instance.getPermsAPI().usePluginBridge) {
+									String[] groups = { minecraftRank };
+									this.instance.getPluginMessenger().sendGroupAction(mcuuid, GroupAction.REMOVE, groups);
+								}else {
+									this.instance.getPermsAPI().removeGroup(mcuuid, minecraftRank);
+								}
+								
+							}else {
+								this.instance.getUniversalServer().getLogger().warning("Something went wrong with removing the Verificationsgroup on Minecraft!");
+							}
+						}
+						
+						this.instance.getVerifyManager().removeVerified(mcuuid);
+						this.instance.getVerifySQL().deleteVerification(disUUID);
+						command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.unlinked", placeholder));
+						return;
+						
+					}
+				}
+				command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.notVerified", placeholder));
+			}else {
+				//VERIFIED AND TRIED IT AGAIN
+				command.reply(this.instance.getDiscordManager().getDiscordMessage("verification.discordAlreadyAuthenticated", placeholder));
+			}
+			return;
+		}
     	//IS NOT VERIFIED
 
     	//COOLDOWN CHECK
